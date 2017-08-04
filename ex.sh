db61520a833f57c7de5a412bfa24e826ccd8249f
gairik <pitonrok@gmail.com>
2017-08-04 11:24:25 +0200

    ex.sh added

---
+++ ex.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Installing RabbitMQ"
+
+echo "removing previous rabbitmqdocker container"
+docker stop rabbitmq
+echo "intalling pika"
+pip install pika
+apt-get update
+apt-get -y upgrade
+echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
+curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
+apt-get update
+apt-get install rabbitmq-server
+rabbitmq-plugins enable rabbitmq_management
+rabbitmqctl trace_on
+rabbitmqctl add_user openstack demo
+rabbitmqctl set_user_tags openstack administrator
+rabbitmqctl set_permissions -p / openstack ".*" ".*" ".*"
+git clone https://gitlab.com/gairik/ENOS
+cd ENOS
+python feed.py
+
+
